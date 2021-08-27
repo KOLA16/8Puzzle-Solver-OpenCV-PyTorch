beeba7c5a3f0f99cba47ce23209ab3d4ae293e06
KOLA16 <igor.kolasa@gmail.com>
2021-08-27 14:05:42 +0200

    Add digit extraction and rocognition

--- modules/puzzles/puzzle_processing.py
+++ modules/puzzles/puzzle_processing.py
@@ -65,6 +65,47 @@ def find_puzzle(frame):
     transformed = four_point_transform(gray, puzzleCnt.reshape(4, 2))
 
     return transformed
+
+def extract_digit(cell):
+
+    empty = True
+
+    # apply automatic thresholding to the cell and then clear any
+    # connected borders that touch the border of the cell
+    thresh = cv2.threshold(cell, 0, 255,
+        cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    thresh = clear_border(thresh)
+
+    # find contours in the thresholded cell
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
     
+    # if no contours were found than this is an empty cell
+    if len(cnts) == 0:
+        return None, empty
+    
+    # otherwise, find the largest contour in the cell and create a
+    # mask for the contour
+    empty = False
+    c = max(cnts, key=cv2.contourArea)
+    mask = np.zeros(thresh.shape, dtype="uint8")
+    cv2.drawContours(mask, [c], -1, 255, -1)
+
+    # compute the percentage of masked pixels relative to the total
+    # area of the image
+    (h, w) = thresh.shape
+    percentFilled = cv2.countNonZero(mask) / float(w * h)
+
+    # if less than 3% of the mask is filled then we are looking at
+    # noise and can safely ignore the contour
+    #if percentFilled < 0.03:
+        #return None
+    
+    # apply the mask to the thresholded cell
+    digit = cv2.bitwise_and(thresh, thresh, mask=mask)
+
+    return digit, empty
+
 
 
--- puzzle_solver.py
+++ puzzle_solver.py
@@ -9,22 +9,21 @@ import numpy as np
 from imutils.video import VideoStream, FPS
 from modules.puzzles.puzzle_processing import PuzzleNotFoundError
 from modules.puzzles.puzzle_processing import find_puzzle
+from modules.puzzles.puzzle_processing import extract_digit
 
 # construct argument parser
-#ap = argparse.ArgumentParser()
-#ap.add_argument('-m', '--model', required=True,
-    #help='path to file containing classifier model')
-#args = vars(ap.parse_args())
+ap = argparse.ArgumentParser()
+ap.add_argument('-m', '--model', required=True,
+    help='path to file containing classifier model')
+args = vars(ap.parse_args())
 
 # set the device we will be using to run the model
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-#print('model' in args.keys())
-
 # load our pretrained model
-#model = torch.load(args['model'])
-#model.to(DEVICE)
-#model.eval()
+model = torch.load(args['model'])
+model.to(DEVICE)
+model.eval()
 
 # define the list of labels
 labels = '0123456789'
@@ -39,7 +38,7 @@ time.sleep(2.0)
 while True:
 
     # grab the frame from the threaded video stream
-    _, frame = camera.read()    
+    frame = camera.read()[1]    
     
     try:
         transformed_puzzle = find_puzzle(frame)
@@ -55,62 +54,49 @@ while True:
         continue
     else:
         # rest of the operations
+        digit, empty = extract_digit(transformed_puzzle)
 
-        # show the output frame
-        cv2.imshow('Puzzle Solver', frame)
-        key = cv2.waitKey(1) & 0xFF
+        if empty:
+            # show the output frame
+            cv2.imshow('Puzzle Solver', frame)
+            key = cv2.waitKey(1) & 0xFF
 
-        # if the 'q' key was pressed, break from the loop
-        if key == ord('q'):
-            break
+            # if the 'q' key was pressed, break from the loop
+            if key == ord('q'):
+                break
 
-    """digit_show = cv2.threshold(transformed_puzzle, 0, 255, 
-        cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-    digit_show = clear_border(digit_show)
+            continue
+        else:
+            # resize to 28x28
+            digit = cv2.resize(digit, (28, 28))
 
-        #cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-            #cv2.CHAIN_APPROX_SIMPLE)
-        #cnts = imutils.grab_contours(cnts)
+            # add a batch dimension, grayscale channel dimension
+            # and convert the frame to a floating point tensor
+            digit = np.expand_dims(digit, axis=0)
+            digit = np.expand_dims(digit, axis=0)
+            digit = digit / 255.0
+            digit = torch.FloatTensor(digit)
 
-        #c = max(cnts, key=cv2.contourArea)
-        #mask = np.zeros(thresh_roi.shape, dtype="uint8")
-        #cv2.drawContours(mask, [c], -1, 255, -1)
+            # send the input to the device and pass the it through the
+            # network to get the label prediction
+            digit = digit.to(DEVICE)
+            probabilities = model(digit)
 
-        #digit_show = cv2.bitwise_and(thresh_roi, thresh_roi, mask=mask)
+            # find the label
+            max_prob_idx = torch.argmax(probabilities, dim=1).item()
+            label = labels[max_prob_idx]
 
-        # compute the bounding box
-        #(x, y, w, h) = cv2.boundingRect(digitCnt)
-        #roi = gray[y:y + h, x:x + w]
+            # draw the predcition
+            cv2.putText(frame, label, (350, 35),
+            cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 2)
 
-        #----------------------------------------------------------------------
+            # show the output frame
+            cv2.imshow('Puzzle Solver', frame)
+            key = cv2.waitKey(1) & 0xFF
 
-        # resize to 28x28
-    digit = cv2.resize(digit_show, (28, 28))
-    
-    # add a batch dimension, grayscale channel dimension
-    # and convert the frame to a floating point tensor
-    digit = np.expand_dims(digit, axis=0)
-    digit = np.expand_dims(digit, axis=0)
-    digit = digit / 255.0
-    digit = torch.FloatTensor(digit)
-
-    # send the input to the device and pass the it through the
-    # network to get the label prediction
-    digit = digit.to(DEVICE)
-    probabilities = model(digit)
-
-    # find the label
-    max_prob_idx = torch.argmax(probabilities, dim=1).item()
-    prob = probabilities[0][max_prob_idx].item()
-    prob_text = f'{prob:.2f}'
-    label = labels[max_prob_idx]
-
-    # draw the predcition
-    #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    cv2.putText(frame, label, (x - 10, y - 10),
-        cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 2)
-    cv2.putText(frame, prob_text, (x + 70, y - 10), 
-        cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 2)"""
+            # if the 'q' key was pressed, break from the loop
+            if key == ord('q'):
+                break
 
 # cleanup
 camera.release()
